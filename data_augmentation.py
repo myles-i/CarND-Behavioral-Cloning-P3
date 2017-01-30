12385e85124601635575ca665f86f942331d442a
Myles Iribarne <myles.iribarne@gmail.com>
2017-01-29 18:51:03 -0800

    Everything seems to run but there is no training. Outputs constant angle in autonomous mode

---
+++ data_augmentation.py
@@ -0,0 +1,208 @@
+'''
+Much of this work was inspired by blog posts and code posted by Vivek Yadav:
+- https://chatbotslife.com/using-augmentation-to-mimic-human-driving-496b569760a9#.iijuava2v
+- https://medium.com/@vivek.yadav/dealing-with-unbalanced-data-generating-additional-data-by-jittering-the-original-image-7497fe2119c3#.pjmpsx84p
+'''
+import numpy as np
+import cv2
+from collections import namedtuple
+
+
+AugImg_Vals = namedtuple('AugImg_Vals', ['brightness_scale',
+										'translation_pixels',
+										'added_shadow',
+										'hflipped',
+										'vflipped',
+										'rotation_angle',
+										'shear_angles'])
+class AugImg_Vals:
+	def __init__(self):
+		self.brightness_scale = 1
+		self.translation_pixels = (0,0)
+		self.added_shadow = False
+		self.hflipped = False
+		self.vflipped = False
+		self.rotation_angle = 0
+		self.shear_angles = (0,0)
+
+
+def augment_image_brightness(image, brightness_range = (0.25,1.5), return_rand_param = False):
+	'''
+	Description: applies a random brightness to an image within a specified range
+	inputs:
+	- image: Image in the form of a numpy array in (row, column) format
+	- trans_range: A tuple of size 2 that indicates the range of translations in (y,x)
+	outputs: 
+	- img: The translated image
+	- trans_amount: The percentages by which the image has been translated in y and x
+	'''
+	image1 = cv2.cvtColor(image,cv2.COLOR_RGB2HSV)
+	random_bright =np.random.uniform(brightness_range[0],brightness_range[1])
+	image1[:,:,2] = np.uint8(np.clip(np.float32(image1[:,:,2])*random_bright,0,255)) # saturate at 255
+	image1 = cv2.cvtColor(image1,cv2.COLOR_HSV2RGB)
+	if return_rand_param:
+		return image1, random_bright
+	else:
+		return image1
+
+def trans_image(image, trans_range, return_rand_param = False):
+	'''
+	Description: applies a random translation to an image within a specified range of pixels
+	inputs:
+	- image: Image in the form of a numpy array in (row, column) format
+	- trans_range: A tuple of size 2 that indicates the range of translations in (y,x)
+	outputs: 
+	- img: The translated image
+	- trans_amount: The number of pixels by which the image has been translated in y and x
+	'''
+	rows, cols, ch = image.shape 
+	tr_x = np.random.randint(-trans_range[1],trans_range[1]+1)
+	tr_y = np.random.randint(-trans_range[0],trans_range[0]+1)
+	Trans_M = np.float32([[1,0,tr_x],[0,1,tr_y]])
+	image_tr = cv2.warpAffine(image,Trans_M,(cols,rows))
+	if return_rand_param:
+		return image_tr, (tr_y,tr_x)
+	else:
+		return image_tr
+
+def add_random_shadow(image):
+	'''
+	Description: adds a random shadow to the image
+	inputs:
+	- image: Image in the form of a numpy array in (row, column) format
+	outputs: 
+	- img: The image containing the random shadow
+	'''
+	shp = image.shape
+	top_y = shp[1]*np.random.uniform()
+	
+	top_x = 0
+	bot_x = shp[0]
+	bot_y = shp[1]*np.random.uniform()
+	image_hls = cv2.cvtColor(image,cv2.COLOR_RGB2HLS)
+	shadow_mask = 0*image_hls[:,:,1]
+	X_m = np.mgrid[0:image.shape[0],0:image.shape[1]][0]
+	Y_m = np.mgrid[0:image.shape[0],0:image.shape[1]][1]
+	shadow_mask[((X_m-top_x)*(bot_y-top_y) -(bot_x - top_x)*(Y_m-top_y) >=0)]=1
+	#random_bright = .25+.7*np.random.uniform()
+	if np.random.randint(2)==1:
+		random_bright = .5 # maybe make this brightness parameterized
+		cond1 = shadow_mask==1
+		cond0 = shadow_mask==0
+		if np.random.randint(2)==1:
+			image_hls[:,:,1][cond1] = image_hls[:,:,1][cond1]*random_bright
+		else:
+			image_hls[:,:,1][cond0] = image_hls[:,:,1][cond0]*random_bright    
+	image = cv2.cvtColor(image_hls,cv2.COLOR_HLS2RGB)
+	return image
+
+
+def hflip(img,probability=0.5, return_rand_param = False):
+	'''
+	Description: Flips the image horizontally with a given probability
+	inputs:
+	- img: Image in the form of a numpy array in (row, column) format
+	- probability: The probability that the image will be flipped horizontally (0 to 1)
+	outputs: 
+	- img: The flippped (or un-flipped) image
+	- image_flipped: indicates if the image has been flipped
+	'''
+	flip_image = np.random.uniform()<probability
+	if flip_image:
+		img = cv2.flip(img,0)
+
+	if return_rand_param:
+		return img, flip_image
+	else:
+		return img
+
+
+def vflip(img,probability=0.5, return_rand_param = False):
+	'''
+	Description: Flips the image vertically with a given probability
+	inputs:
+	- img: Image in the form of a numpy array in (row, column) format
+	- probability: The probability that the image will be flipped vertically (0 to 1)
+	outputs: 
+	- img: The flippped (or un-flipped) image
+	- image_flipped: indicates if the image has been flipped
+	'''
+	flip_image = np.random.uniform()<probability
+	if flip_image:
+		img = cv2.flip(img,1)
+
+	if return_rand_param:
+		return img, flip_image
+	else:
+		return img
+
+def rotate_image(img, rot_range=(-15.,15.),return_rand_param = False):
+	'''
+	Description: applies a random rotation to an image within a specified range of degrees
+	inputs:
+	- img: Image in the form of a numpy array in (row, column) format
+	- rot_range: The range of random rotations in degrees draw from a uniform distribution
+	outputs: 
+	- img: The rotated image
+	- ang_rot: Angle by which the image has been rotated
+	'''
+	ang_rot = np.random.uniform(rot_range[0],rot_range[1])
+	rows, cols, ch = img.shape   
+	Rot_M = cv2.getRotationMatrix2D((cols/2,rows/2),ang_rot,1)
+	img = cv2.warpAffine(img,Rot_M,(cols,rows))
+	if return_rand_param:
+		return img, ang_rot
+	else:
+		return img
+
+def shear_image(image, shear_range = (-10,10),return_rand_param = False):
+	'''
+	Description: applies a random shear to an image within a specified range of degrees (y,x)
+	inputs:
+	https://cs.colorado.edu/~mcbryan/5229.03/mail/55.htm
+	- img: Image in the form of a numpy array in (row, column) format
+	- shear_range: The range of random shears in degrees draw from a uniform distribution
+	outputs: 
+	- img: The sheared image
+	- ang_rot: Degrees by which the image has been sheared (y,x)
+	'''
+	row,col,ch = image.shape
+	angleY = np.random.uniform(-shear_range[0],shear_range[0])
+	shear_My = np.array([[1, np.tan(np.radians(angleY))],[0,1]])
+	angleX = np.random.uniform(-shear_range[1],shear_range[1])
+	shear_Mx = np.array([[1, 0],[np.tan(np.radians(angleX)),1]])
+	shear_M = np.dot(shear_My,shear_Mx)
+	affine_trans = np.zeros((2,3))
+	affine_trans[0:2,0:2] = shear_M
+	image = cv2.warpAffine(image,affine_trans,(col,row))
+	if return_rand_param:
+		return image, (angleY,angleX)
+	else:
+		return image
+
+
+def augment_image(image, shear_range = (0,0), rot_range =(0,0),
+				vflip_prob = 0, hflip_prob = 0, add_shadow = False,
+				trans_range = (0,0), brightness_range = (1,1), return_rand_param = False):
+	
+
+	AugImg_Values = AugImg_Vals()
+	
+	image, AugImg_Values.vflipped = vflip(image,probability=vflip_prob, return_rand_param = True)
+	image, AugImg_Values.hflipped = hflip(image,probability=hflip_prob, return_rand_param = True)
+	image, AugImg_Values.brightness_scale = augment_image_brightness(image, brightness_range = brightness_range, return_rand_param = True)
+	image, AugImg_Values.shear_angles= shear_image(image, shear_range = shear_range,return_rand_param = True)
+	image, AugImg_Values.rotation_angle= rotate_image(image, rot_range=rot_range,return_rand_param = True)
+	image, AugImg_Values.translation_pixels = trans_image(image, trans_range, return_rand_param = True)
+
+	if add_shadow:
+		image = add_random_shadow(image)
+		AugImg_Values.added_shadow = True
+
+	if return_rand_param:
+		return image, AugImg_Values
+	else:
+		return image
+
+
+	
--- model.py
+++ model.py
@@ -1,53 +1,114 @@
 ##################
-# Preprocessing
+# Load csv
 ##################
-from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import array_to_img, img_to_array, load_img
+import numpy as np
+import data_augmentation as da
+import pandas as pd 
+import cv2
+csv_data = pd.read_csv('./driving_log.csv')
+input_shape = (160,320,3)
 
-# Create an ImageData Generator that resclales the image from [0,1] and does some data augmentation
-train_datagen = ImageDataGenerator(
-        rotation_range=10, #degrees
-        width_shift_range=0.0,
-        height_shift_range=0.1,
-        rescale=1./255, # rescale to between 0 and 1
-        shear_range=0.1, # 2.8 degree sheer
-        zoom_range=0.2,
-        fill_mode='nearest')
-# test datagen only rescale the test data images
-batch_size = 128
-test_datagen = ImageDataGenerator(rescale=1./255) 
 
-train_generator = train_datagen.flow(
-        X = X_train,
-        y = y_train,
-        batch_size=batch_size)
+##################
+# Reserve some data for validation
+##################
+nb_val_samples = len(csv_data) *0.2 #reserve 20% for validation
+val_idx = np.random.randint(len(csv_data), size = nb_val_samples)
+val_idx = tuple(val_idx)
 
-# this is a similar generator, for validation data
-validation_generator = test_datagen.flow(
-        X = X_val,
-        y = y_val,
-        batch_size=batch_size)
 
 #######################
+# Define data generators and processors
+#######################
+def grab_data(csv_data, index, camera):
+    shift_val = 0.2
+    if camera == 0: #left camera
+        image= img_to_array(load_img(csv_data['left'][index].lstrip()))
+        steering_cmd = csv_data['steering'][index] + shift_val# shift to simulate this as the center camera
+    elif camera == 1: #center camera
+        image= img_to_array(load_img(csv_data['center'][index].lstrip()))
+        steering_cmd = csv_data['steering'][index]
+    elif camera == 2: #right camera
+        image= img_to_array(load_img(csv_data['right'][index].lstrip()))
+        steering_cmd = csv_data['steering'][index] - shift_val # shift to simulate this as the center camer
+
+    return image,steering_cmd
+
+
+def augment_data(image, steering_cmd):
+    # augment image (randomly flip about vertical axis, randomly translate image,
+    # randomly change brightness, randomly add shadows)
+    
+    image, AugImg_Values = da.augment_image(image, shear_range = (0,0), rot_range =(0,0),
+                                            vflip_prob = 0.5, hflip_prob = 0, add_shadow = True,
+                                            trans_range = (20,20), brightness_range = (0.25,1.5), return_rand_param = True)
+    # now augment steering_cmd based on how image was augmented
+    steering_per_pixel = 0.004
+    steering_cmd = steering_cmd + steering_per_pixel*AugImg_Values.translation_pixels[1]
+    
+    if AugImg_Values.vflipped:
+        steering_cmd = -steering_cmd
+    
+    return image, steering_cmd
+
+def my_train_datagen(csv_data,val_idxs, batch_size = 128):
+    batch_X = np.zeros((batch_size,) + input_shape)
+    batch_y = np.zeros(batch_size)
+    while True:
+        for i in range(batch_size):
+            steering_cmd = 0 #initialize to greater than 0.1
+            while abs(steering_cmd)<0.1:
+                #randomize which image is used, but not from validation set
+                random_index = np.random.randint(len(csv_data))
+                while val_idxs.__contains__(random_index):
+                    random_index = np.random.randint(len(csv_data))
+
+                #randomize which camera to use
+                random_camera = np.random.randint(3)
+
+                image, steering_cmd = grab_data(csv_data,random_index,random_camera)
+                image, steering_cmd = augment_data(image, steering_cmd) 
+
+
+            batch_X[i] = image
+            batch_y[i] = steering_cmd
+        yield (batch_X, batch_y)
+
+def my_validation_datagen(csv_data, val_idxs, batch_size = 128): 
+    batch_X = np.zeros((batch_size,) + input_shape)
+    batch_y = np.zeros(batch_size)
+    counter = 0
+    while True:
+        for i in range(batch_size):
+            counter+=1
+            #cycle through validation data
+            index = val_idxs[counter%len(val_idxs)]
+            camera = 1 #always center camera for validation
+
+            batch_X[i], batch_y[i] = grab_data(csv_data,index,camera)
+        yield (batch_X, batch_y)
+#######################
 # Build network
-#####################
+#######################
 from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.core import Dense, Dropout, Activation, Flatten, Lambda
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.utils import np_utils
+from keras.optimizers import Adam
 
 #Define convolutional layer
-input_shape = (160,320,3)
-dropout = 0.1 # percent that will dropout
+dropout = 0# percent that will dropout
 
 
 model = Sequential()
+# model.add(Lambda( lambda x: x/255.0-0.5, input_shape=input_shape))
 ####Convolution Layer 
 model.add(Convolution2D(24, 5, 5,
 						subsample = (2,2),
-                        border_mode='valid',
-                        input_shape=input_shape))
+                        border_mode='valid', input_shape=input_shape))
 #model.add(MaxPooling2D(pool_size=pool_size))
-model.add(Dropout(dropout))
+# model.add(Dropout(dropout))
 model.add(Activation('relu'))
 
 ####Convolution Layer 2
@@ -55,7 +116,7 @@ model.add(Convolution2D(36, 5, 5,
                         border_mode='valid',
                         subsample = (2,2)))
 # model.add(MaxPooling2D(pool_size=pool_size))
-model.add(Dropout(dropout))
+# model.add(Dropout(dropout))
 model.add(Activation('relu'))
 
 ####Convolution Layer 3
@@ -63,7 +124,7 @@ model.add(Convolution2D(48, 5, 5,
                         border_mode='valid',
                         subsample = (2,2)))
 # model.add(MaxPooling2D(pool_size=pool_size))
-model.add(Dropout(dropout))
+# model.add(Dropout(dropout))
 model.add(Activation('relu'))
 
 ####Convolution Layer 4
@@ -71,7 +132,7 @@ model.add(Convolution2D(64, 3, 3,
                         border_mode='valid',
                         subsample = (1,1)))
 model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(dropout))
+# model.add(Dropout(dropout))
 model.add(Activation('relu'))
 
 ####Convolution Layer 5
@@ -79,7 +140,7 @@ model.add(Convolution2D(64, 3, 3,
                         border_mode='valid',
                         subsample = (1,1)))
 model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Dropout(dropout))
+# model.add(Dropout(dropout))
 model.add(Activation('relu'))
 
 ## Fully Connected Layers
@@ -91,14 +152,16 @@ model.add(Activation('relu'))
 model.add(Dense(10))
 model.add(Activation('relu'))
 model.add(Dense(1))
-model.compile(loss='mean_squared_error', optimizer='Adam')
+Adamoptimizer = Adam(lr=0.001)
+model.compile(loss='mean_squared_error', optimizer=Adamoptimizer)
 model.summary()
 
 
 
-# conv_model = model
+# # conv_model = model
 # model = Sequential()
-# model.add(Flatten(input_shape=img_size))
+# model.add(Lambda( lambda x: x/255.0-0.5, input_shape=input_shape))
+# model.add(Flatten())
 # model.add(Dense(128))
 # model.add(Activation('relu'))
 # model.add(Dense(128))
@@ -111,10 +174,16 @@ print('Model Compiled...\n')
 # Train
 #########
 print('Training...')
-nb_epoch = 6
-history = model.fit(np.squeeze(X_train), y_train,
-                    batch_size=batch_size, nb_epoch=nb_epoch,
-                    verbose=1, validation_data = (X_val,y_val))
+nb_epoch = 3
+samples_per_epoch = 10000
+
+training_gen = my_train_datagen(csv_data,val_idx, batch_size = 128)
+validation_gen = my_validation_datagen(csv_data,val_idx, batch_size = 128)
+history = model.fit_generator(training_gen, 
+                    samples_per_epoch=samples_per_epoch, nb_epoch=nb_epoch,
+                    verbose=1, validation_data = validation_gen, nb_val_samples=nb_val_samples)
+
+
 
 ##############
 # Save to Disc
